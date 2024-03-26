ab1b1aed499613732b5d3912278e0a95c6ac63a2
AmadeusWM <63149896+AmadeusWM@users.noreply.github.com>
2024-03-26 18:54:02 +0100

    fix output

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
-  outputs = {
+  outputs = {nixpkgs, ...}@inputs: {
     templates = {
       empty = {
         path = "./empty";
