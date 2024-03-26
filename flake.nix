d785939cd8fda694f5e8b15c8a10da617f022739
AmadeusWM <63149896+AmadeusWM@users.noreply.github.com>
2024-03-26 19:11:08 +0100

    provide script

--- flake.nix
+++ flake.nix
@@ -3,7 +3,26 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
-  outputs = {nixpkgs, ...}@inputs: {
+  outputs = {nixpkgs, ...}@inputs: 
+  let
+    supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+    forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
+      pkgs = import nixpkgs { inherit system; };
+    });
+  in {
+    packages = forEachSupportedSystem ({pkgs}: {
+      default = pkgs.writeShellScriptBin "nixdev" ''
+        echo "hello"
+        # if [ -z "$1" ]; then
+        #   echo "missing dev template argument, you can choose from: clojure csharp cue dhall elixir elm gleam go hashi haskell haxe java kotlin latex nickel nim nix node ocaml opa php protobuf pulumi purescript python ruby rust-toolchain rust scala shell zig"
+        #   nix flake new -t github:nix-community/nix-direnv .
+        # else
+        #   nix flake init --template github:the-nix-way/dev-templates#$1
+        # fi
+        # direnv allow .
+        # cat .gitignore | grep ".direnv" || echo ".direnv" >> .gitignore
+    '';
+    });
     templates = {
       empty = {
         path = ./empty;
